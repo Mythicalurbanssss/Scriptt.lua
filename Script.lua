3e8718621e2d0df02c9d78b0e52a83a2e3fb5779
Mythicalurbanssss <101818698+Mythicalurbanssss@users.noreply.github.com>
2022-03-17 16:04:05 -0400

    File.lua

---
+++ Script.lua
@@ -0,0 +1,104 @@
+local args = {
+    [1] = "GUYS IM HACKING!!! ATTENTION I AM HACKING. I AM A DISGRACE TO ROBLOX AND ITS PLAYERS!!!",
+    [2] = "All",
+}
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+wait(0.5)
+local args = {
+    [1] = "GUYS IM HACKING!!! ATTENTION I AM HACKING. I AM A DISGRACE TO ROBLOX AND ITS PLAYERS!!!",
+    [2] = "All",
+}
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+wait(0.5)
+local args = {
+    [1] = "GUYS IM HACKING!!! ATTENTION I AM HACKING. I AM A DISGRACE TO ROBLOX AND ITS PLAYERS!!!",
+    [2] = "All",
+}
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+wait(0.5)
+local args = {
+    [1] = "GUYS IM HACKING!!! ATTENTION I AM HACKING. I AM A DISGRACE TO ROBLOX AND ITS PLAYERS!!!",
+    [2] = "All",
+}
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+wait(0.5)
+local args = {
+    [1] = "GUYS IM HACKING!!! ATTENTION I AM HACKING. I AM A DISGRACE TO ROBLOX AND ITS PLAYERS!!!",
+    [2] = "All",
+}
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+wait(0.5)
+local args = {
+    [1] = "GUYS IM HACKING!!! ATTENTION I AM HACKING. I AM A DISGRACE TO ROBLOX AND ITS PLAYERS!!!",
+    [2] = "All",
+}
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+wait(0.5)
+local args = {
+    [1] = "GUYS IM HACKING!!! ATTENTION I AM HACKING. I AM A DISGRACE TO ROBLOX AND ITS PLAYERS!!!",
+    [2] = "All",
+}
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+wait(0.5)
+local args = {
+    [1] = "GUYS IM HACKING!!! ATTENTION I AM HACKING. I AM A DISGRACE TO ROBLOX AND ITS PLAYERS!!!",
+    [2] = "All",
+}
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+wait(0.5)
+local args = {
+    [1] = "GUYS IM HACKING!!! ATTENTION I AM HACKING. I AM A DISGRACE TO ROBLOX AND ITS PLAYERS!!!",
+    [2] = "All",
+}
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+wait(0.5)
+local args = {
+    [1] = "GUYS IM HACKING!!! ATTENTION I AM HACKING. I AM A DISGRACE TO ROBLOX AND ITS PLAYERS!!!",
+    [2] = "All",
+}
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+wait(0.5)
+local args = {
+    [1] = "GUYS IM HACKING!!! ATTENTION I AM HACKING. I AM A DISGRACE TO ROBLOX AND ITS PLAYERS!!!",
+    [2] = "All",
+}
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+wait(0.5)
+local args = {
+    [1] = "GUYS IM HACKING!!! ATTENTION I AM HACKING. I AM A DISGRACE TO ROBLOX AND ITS PLAYERS!!!",
+    [2] = "All",
+}
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+wait(0.5)
+local args = {
+    [1] = "GUYS IM HACKING!!! ATTENTION I AM HACKING. I AM A DISGRACE TO ROBLOX AND ITS PLAYERS!!!",
+    [2] = "All",
+}
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+wait(0.5)
+local args = {
+    [1] = "GUYS IM HACKING!!! ATTENTION I AM HACKING. I AM A DISGRACE TO ROBLOX AND ITS PLAYERS!!!",
+    [2] = "All",
+}
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+wait(0.5)
+local args = {
+    [1] = "GUYS IM HACKING!!! ATTENTION I AM HACKING. I AM A DISGRACE TO ROBLOX AND ITS PLAYERS!!!",
+    [2] = "All",
+}
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
